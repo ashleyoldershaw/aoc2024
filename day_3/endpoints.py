862228f42091747afa069fc1f4e75fe8632f0685
ashleyoldershaw <ash.oldershaw@gmail.com>
2024-12-04 08:16:49 +0000

    day 3 complete

---
+++ day_3/endpoints.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter
+
+from aoc_types import TaskInput
+import re
+
+day_3_routes = APIRouter()
+
+
+def extract_mul_values(mul_string: str):
+    return re.findall(r"mul\([1-9][0-9]*,[1-9][0-9]*\)", mul_string)
+
+
+def process_mul_strings(mul_string: str):
+    mul_lists = extract_mul_values(mul_string)
+    # go through each mul value, take out the numbers and convert to integers
+    values_to_multiply = [
+        [m for m in map(int, mul.strip("mul()").split(","))] for mul in mul_lists
+    ]
+    # multiply each list together and add up the values
+    total = sum(map(lambda x: x[0] * x[1], values_to_multiply))
+    return total
+
+
+def slice_out_ignored_sequences(mul_string: str):
+    # split on don't values
+    # if they contain a 'do', cut everything after that and add it to the return string
+    # if there is no 'do' then it's don't followed by don't, so everything inbetween is to be cut
+    split_on_dont = mul_string.split("don't()")
+    to_add = [split_on_dont[0]]
+    for i in range(1, len(split_on_dont)):
+        a = split_on_dont[i].split("do()")
+        if len(a) > 1:
+            to_add.append(" ".join(a[1:]))
+
+    return " ".join(to_add)
+
+
+@day_3_routes.post("/1")
+async def task_1(task_input: TaskInput):
+    total = process_mul_strings(re.sub("\n", " ", task_input.data))
+
+    return {"answer": total}
+
+
+@day_3_routes.post("/2")
+async def task_2(task_input: TaskInput):
+    print(slice_out_ignored_sequences(task_input.data))
+    total = process_mul_strings(slice_out_ignored_sequences(task_input.data))
+
+    return {"answer": total}
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from day_1.endpoints import day_1_routes
 from day_2.endpoints import day_2_routes
+from day_3.endpoints import day_3_routes
 
 app = FastAPI()
 # Register the router
 app.include_router(day_1_routes, prefix="/1")
 app.include_router(day_2_routes, prefix="/2")
+app.include_router(day_3_routes, prefix="/3")
